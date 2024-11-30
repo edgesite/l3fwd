d40663c17b10e565e0faaf7bad52bd73828a5f94
KITAHARA SETSUNA <edgesite@protonmail.com>
2024-11-30 12:23:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# l3fwd
+
+A simple L3 forwarding acceleration POC. It's an alternative implementation of the host chain. When I deploy a full-mesh network, I found that MLX's host chain has issues with opening circuits. So I run FRR and use the l3fwd program to accelerate forwarding, check https://github.com/edgesite/l3fwd/tree/main/lab for details. Compared to MLX's host chain, this program achieves high-performance forwarding on generic hardware and can integrate with common routing programs (such as FRR).
+
+This program reads /32 routes from the kernel routing table, synthesizes srcmac, dstmac, and ifindex, and installs them into the BPF map. When receiving packets with known destination addresses, it bypasses the kernel network stack and directly redirects them.
+
+## Known Issues
+
+- On virtio-net, the program only works under xdpgeneric mode.
+- Limited Jumbo Frame support: https://github.com/search?q=repo%3Atorvalds%2Flinux+XDP_REDIRECT+frags&type=commits
+
+## Dependencies
+
+- `apt install clang llvm libbpf-dev linux-headers-$(uname -r)`
+- `go get github.com/cilium/ebpf/cmd/bpf2go`
+
+## Build
+
+```sh
+sysctl net.ipv4.ip_forward=1
+sysctl net.ipv4.conf.default.rp_filter=0
+go generate
+GOARCH=amd64 go run ./ ens20
+```
---
+++ xdp/l3fwd.c
@@ -0,0 +1,58 @@
+#include <bpf/bpf_endian.h>
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <asm/byteorder.h>
+#include <bpf/bpf_helpers.h>
+
+struct route_info {
+    unsigned char src_mac[ETH_ALEN];
+    unsigned char dst_mac[ETH_ALEN];
+    __u32 ifindex;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, __u32);
+    __type(value, struct route_info);
+    __uint(max_entries, 10000);
+} route_map SEC(".maps");
+
+SEC("xdp")
+int l3fwd(struct xdp_md *ctx) {
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+    
+    struct ethhdr *eth = data;
+    if ((void*)(eth + 1) > data_end)
+        return XDP_PASS;
+
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    struct iphdr *iph = (void*)(eth + 1);
+    if ((void*)(iph + 1) > data_end)
+        return XDP_PASS;
+
+    struct route_info *info = bpf_map_lookup_elem(&route_map, &iph->daddr);
+    if (!info) {
+        // bpf_printk("l3fwd: No route info found for destination IP: %pI4", &iph->daddr);
+        return XDP_PASS;
+    }
+
+    if (info->ifindex != ctx->ingress_ifindex) {
+        __builtin_memcpy(eth->h_source, info->src_mac, ETH_ALEN);   
+        __builtin_memcpy(eth->h_dest, info->dst_mac, ETH_ALEN);
+        // bpf_printk("l3fwd: redirecting %pI4 to interface %d, src interface %d", &iph->daddr, info->ifindex, ctx->ingress_ifindex);
+        // bpf_printk("l3fwd: rewritten SMAC: %x:%x:%x:%x:%x:%x", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
+        // bpf_printk("l3fwd: rewritten DMAC: %x:%x:%x:%x:%x:%x", eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+
+        long rc = bpf_redirect(info->ifindex, 0);
+        // bpf_printk("l3fwd: redirect res=%ld", rc);
+        return rc;
+    }
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
